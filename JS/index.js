4b7d10b1c15fd14b3536bf388d72d7abb87162a9
TareadeCursoProduccion <165443407+TareadeCursoProduccion@users.noreply.github.com>
2024-06-25 13:59:57 -0600

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const categoriesContainer = document.querySelector(".categories-container");
     const buttons = document.querySelectorAll(".round-button");
 
-   
+   window.reload();
 
     if(usuarioInput)
         {
